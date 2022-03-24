f401d7c41e032ef800a248f79f4d597bdb1a0154
kedhavp30 <kedhav.pusun30@gmail.com>
2022-03-24 13:11:30 +0400

    Sidebar for admin and user(User P)/Edit addedit
    Co-authored-by: hdsmathew <hdsmathew@users.noreply.github.com>

--- adminfolder/js/nav.js
+++ adminfolder/js/nav.js
@@ -1,62 +1,62 @@
 const createNav = () => {
-  let nav = document.querySelector(".sidebar");
+  let nav = document.querySelector(".mysidebar");
 
   nav.innerHTML =`
     <div class="logo-details">
       <i class='bx bxl-a-plus-plus'></i>
       <span class="logo_name">ADMIN</span>
     </div>
-    <ul class="nav-links">
+    <ul class="mynav-links">
       <li>
-        <a href="#" class="active">
+        <a href="#" class="myactive">
           <i class='bx bx-grid-alt' ></i>
-          <span class="links_name">Dashboard</span>
+          <span class="mylinks_name">Dashboard</span>
         </a>
       </li>
       <li>
         <a href="#">
           <i class='bx bx-box' ></i>
-          <span class="links_name">Product</span>
+          <span class="mylinks_name">Product</span>
         </a>
       </li>
       <li>
         <a href="#">
           <i class='bx bx-list-ul' ></i>
-          <span class="links_name">Order list</span>
+          <span class="mylinks_name">Order list</span>
         </a>
       </li>
       <li>
         <a href="#">
           <i class="bx bx-comment-detail"></i>
-          <span class="links_name">Reviews</span>
+          <span class="mylinks_name">Reviews</span>
         </a>
       </li>
       <li>
         <a href="#">
           <i class='bx bx-message' ></i>
-          <span class="links_name">Feedbacks</span>
+          <span class="mylinks_name">Feedbacks</span>
         </a>
       </li>
       <li>
         <a href="#">
           <i class='bx bx-user' ></i>
-          <span class="links_name">Registered Users</span>
+          <span class="mylinks_name">Registered Users</span>
         </a>
       </li>
       <li class="log_out">
         <a href="#">
           <i class='bx bx-log-out'></i>
-          <span class="links_name">Log out</span>
+          <span class="mylinks_name">Log out</span>
         </a>
       </li>
     </ul>
 `;
 
-  let sidebar = document.querySelector(".sidebar");
+  let sidebar = document.querySelector(".mysidebar");
   let sidebarBtn = document.querySelector(".sidebarBtn");
   sidebarBtn.onclick = function () {
-    sidebar.classList.toggle("active");
-    if (sidebar.classList.contains("active")) {
+    sidebar.classList.toggle("myactive");
+    if (sidebar.classList.contains("myactive")) {
       sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
     } else sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
   };
