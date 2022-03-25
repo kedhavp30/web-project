bf70f121b987cfcedf7986667ecd6ae85146afa6
hdsmathew <chnmathew@gmail.com>
2022-03-25 21:19:38 +0400

    review php

--- public/review.php
+++ public/review.php
@@ -1,11 +1,100 @@
 <?php
-session_start();
+  session_start();
 
-if(!isset($_SESSION['username']))
-{
-  header("Location: viewproduct.html referer=review");
-}
+  if (!isset($_SESSION["username"])) {
+    header("Location: signin.php?referer=review");
+    die();
+  }
+
+  $username = $rate = $comment = "";
+  $rate = $commentErr = "";
+
+  $username = $_SESSION["username"];
+
+  require_once "includes/db_connect.php";
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // From viewproduct page
+  if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $_SESSION["productid"] = $_GET["productid"];
+    $_SESSION["size"] = $_GET["size"];
+    $_SESSION["colour"] = $_GET["colour"];
+
+    $productQuery = "SELECT * FROM orders 
+                    INNER JOIN orderitems ON orders.orderid = orderitems.orderid
+                    WHERE orders.username = {$conn->quote($username)}
+                    AND orderitems.productid = {$conn->quote($_SESSION["productid"])}
+                    AND orderitems.size = {$conn->quote($_SESSION["size"])}
+                    AND orderitems.colour = {$conn->quote($_SESSION["colour"])}
+                    AND orderitems.reviewed = 0;";
+
+    $productQueryResult = $conn->query($productQuery);
+    // User didn't buy that product or already reviewed
+    if ( !$productQueryResult->fetchColumn() ) {
+      header("Location: viewproduct.html?productid={$_SESSION["productid"]}&size={$_SESSION["size"]}&colour={$_SESSION["colour"]}");
+      die();
+    }
+  }
+
+  // Form submitted
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["comment"])) {
+      $commentErr = "Required comment";
+    } else {
+      $comment = $_POST["comment"];
+    }    
+    if (empty($_POST["rate"])) {
+      $rateErr = "Required rating";
+    } else {
+      $rate = $_POST["rate"];
+    }
+    
+    
+    // Form valid
+    if ($rateErr == "" && $commentErr == "") {
+
+      $customerid = $conn->query("SELECT customerid FROM customer WHERE username = {$conn->quote($username)}")
+                    ->fetch(PDO::FETCH_ASSOC)["customerid"];
+
+      $reviewQuery = "INSERT INTO review(productId, postedOn, reviewDesc, flag, rating, customerid)
+                      VALUES ({$conn->quote($_SESSION["productid"])}, {$conn->quote(date("Y-m-d"))}, {$conn->quote($comment)}, 0, {$rate}, {$customerid})";
+
+      $updateOrderItemQuery = "UPDATE orderitems
+                              SET reviewed = 1
+                              WHERE orderid IN (SELECT orderid
+                                                FROM orders
+                                                WHERE username = {$conn->quote($username)}
+                                                AND orders.status = 'Delivered')
+                              AND productid = {$conn->quote($_SESSION["productid"])}
+                              AND size = {$conn->quote($_SESSION["size"])}
+                              AND colour = {$conn->quote($_SESSION["colour"])}
+                              AND reviewed = 0";
+
+      $conn->beginTransaction();
+
+      $reviewQueryResult = $conn->exec($reviewQuery);
+      if (!$reviewQueryResult) {
+        // echo "Couldn't submit review.";
+        $conn->rollBack();
+        die();
+      } else {
+        $updateOrderItemQueryResult = $conn->exec($updateOrderItemQuery);
+        if (!$updateOrderItemQueryResult) {
+          // echo "Review submission failed.";
+          $conn->rollBack();
+          die();
+        }
+      }
+
+      $conn->commit();
+      $conn = null;
+
+      header("Location: viewproduct.html?productid={$_SESSION["productid"]}&size={$_SESSION["size"]}&colour={$_SESSION["colour"]}");
+    }
+  }
+
 ?>
+
 
 <html lang="en">
 <head>
@@ -22,120 +111,31 @@ if(!isset($_SESSION['username']))
 
     <nav class="mynavbar"></nav>
 
-    <?php  
-  
-    $usernameErr  = $genderErr = $starratingErr =  $commentErr = "";
-    $username  = $gender = $starrating = $comment  = "";
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      if (empty($_POST["txt_name"])) {
-          $nameErr = "Username is required";
-      } else {
-          $name = test_input($_POST["txt_name"]);
-      }
-      if (empty($_POST["txt_gender"])) {
-        $genderErr = "Gender is required";
-      } else {
-        $gender = test_input($_POST["txt_gender"]);
-      }
-      if (empty($_POST["rate"])) {
-        $genderErr = "Rate is required";
-      } else {
-        $gender = test_input($_POST["rate"]);
-      }
-      if (empty($_POST["txt_comment"])) {
-        $genderErr = "Comment is required";
-      } else {
-        $gender = test_input($_POST["txt_comment"]);
-      }
-
-      if($usernameErr == "" && $clean_ratingErr == "" && $genderErr == "" && $commentErr == "" ){
-        require_once "includes/db_connect.php";
-
-        $sInsert = "INSERT INTO review(productId, postedOn, reviewDesc, flag, rating, customerid)
-        VALUES ".$conn->quote($username) .",".$conn->quote($gender) .",".$conn->quote($rating).
-        ",".$conn->quote($comment) .",'$product_id', '$postedOn', '$reviewDesc', '$flag', '$customerid' .")";
-
-        //Set PDO to exception Mode
-        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $addResult = $conn->exec($sInsert);
-        if($addResult)
-        {
-          echo "Record Saved";
-        }
-        else{
-          echo "Record has not been saved.";
-        }
-    }
-  ?>
-
-  function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }  
-
-
     <div class="review-form">
         <h3 style = "color: black; background-color: beige;">Please review your product</h3>
 
-        <?php
-        require_once "includes/db_connect.php";
-        $sQuery = "SELECT product.productId, review.postedOn, review.reviewDesc, review.flag, review.rating, customer.customerid
-                   FROM review INNER JOIN product.productid = review.productid
-                   INNER JOIN product ON product.productid = review.productid
-                   INNER JOIN product ON customer.customerid = customer.productid
-                   WHERE account.username = " .$conn->quote($_SESSION['username']);
-
-        $Result = $conn->query($sQuery);
-        $numrows = $Result->rowCount();
-        if ($numrows ==0)
-      {
-   	     echo "You have not make a review";
-      }
-      ?>
-
-
-       <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>"  >
-        <fieldset class="personal-details">
-          <legend>Personal Details:</legend>
-          Username :
-          <input type="text" value="" name="txt_name" title="Name should consist of one or more words, starting with an uppercase letter followed by lowercase characters, and separated by spaces" maxlength="16" size="50" pattern="[A-Z][a-z]+( [A-Z][a-z]+)*$" required /><br>
-          Gender:
-          <input type="radio" name="txt_gender" value="male" required/> Male
-          <input type="radio" name="txt_gender" value="female" /> Female
-        </fieldset>
-        
-        <fieldset class="product-details">
-          <legend>Product Details:</legend>
-          <h5>Please give us feedback on your appreciation of the product<h5>
-          <div class="star-rating">
-            <input type="radio" id="star5" name="rate" value="5" />
-            <label for="star5" title="text"></label>
-            <input type="radio" id="star4" name="rate" value="4" />
-            <label for="star4" title="text"></label>
-            <input type="radio" id="star3" name="rate" value="3" />
-            <label for="star3" title="text"></label>
-            <input type="radio" id="star2" name="rate" value="2" />
-            <label for="star2" title="text"></label>
-            <input type="radio" id="star1" name="rate" value="1" />
-            <label for="star1" title="text"></label>
-          </div>
-
-          Comment: </br>
-          <textarea  rows="10" cols="60" name="txt_comment"></textarea><br/><br/>
-          
-          <input type ="submit" style="color:white; background: #00ACEE;  width: 20%; height:30px; text-align: center; " value="  Submit Review" >
-          
-          
-        </fieldset> 
+        <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+            <fieldset class="product-details">
+            <legend>Review:</legend>
+            <h5>Please give us feedback on your appreciation of the product<h5>
+              <div class="star-rating">
+                <input type="radio" id="star5" name="rate" value="5" <?php if ($rate == 5) echo "checked"; ?>/>
+                <label for="star5" title="text"></label>
+                <input type="radio" id="star4" name="rate" value="4" <?php if ($rate == 4) echo "checked"; ?>/>
+                <label for="star4" title="text"></label>
+                <input type="radio" id="star3" name="rate" value="3" <?php if ($rate == 3) echo "checked"; ?>/>
+                <label for="star3" title="text"></label>
+                <input type="radio" id="star2" name="rate" value="2" <?php if ($rate == 2) echo "checked"; ?>/>
+                <label for="star2" title="text"></label>
+                <input type="radio" id="star1" name="rate" value="1" <?php if ($rate == 1) echo "checked"; ?>/>
+                <label for="star1" title="text"></label>
+              </div>
+            Comment: </br>
+            <textarea  rows="10" cols="60" name="comment"><?php echo $comment; ?></textarea><br/><br/>
+            <input type ="submit" style="color:white; background: #00ACEE;  width: 20%; height:30px; text-align: center;" value="Submit Review" ></input>
+          </fieldset> 
         </form>
-      
-         
-        </p>
-        </div>
+      </div>
 
 
     <footer></footer>
@@ -144,3 +144,6 @@ if(!isset($_SESSION['username']))
     <script src="js/footer.js"></script>
     <script src="js/home.js"></script>
     <script src="js/product.js"></script>
+
+</body>
+</html>
