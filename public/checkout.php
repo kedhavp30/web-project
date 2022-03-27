e69012d3c59cad62bf6f3b316397ff5a4f4cf735
hdsmathew <chnmathew@gmail.com>
2022-03-27 17:08:35 +0400

    to add referer register/signin for cart

--- public/checkout.php
+++ public/checkout.php
@@ -1,33 +1,50 @@
 <?php
-session_start();
+	session_start();
 
-if (!isset($_SESSION["username"])) {
-  header("Location: signin.php?referer=checkout");
-  die();
-}
+	if (!isset($_SESSION["username"])) {
+		header("Location: signin.php?referer=checkout");
+		die();
+	}
 
-   $username = $paymentmethod = $creditcardnum = $creditcardpin = "";
-   $paymentmethodErr = $creditcardnumErr = $creditcardpinErr = "";
+	// Initialize empty array if it doens't exist yet
+	if (!isset($_SESSION["cart"])) {
+		$_SESSION["cart"] = array();
+	}
 
-   $username = $_SESSION["username"];
-  
-   require_once "includes/db_connect.php";
-   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$username = $creditcardnum = $creditcardpin = "";
+	$creditcardnumErr = $creditcardpinErr = "";
 
-    //Retrive From Cart Table
-  if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    
+	$username = $_SESSION["username"];
 
-    $cartQuery = "SELECT * FROM cart 
-    WHERE username  = {$conn->quote($_SESSION["username"])}";
+	require_once "includes/db_connect.php";
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+	// Registered users; retrieve cart items from db if not yet
+	if (!$_SESSION["cart"]) {
 
+		$cartQuery = "SELECT cart.productId, size, colour, quantity, cart.unitPrice, cart.discount, prodName, picture
+									FROM cart INNER JOIN product ON cart.productId = product.productId
+									WHERE username = {$conn->quote($username)};";
 
+		$cartQueryResult = $conn->query($cartQuery);
 
-    $cartQueryResult = $conn->query($cartQuery);
-    $_SESSION["cartinfo"] = $cartQueryResult->fetchAll(PDO::FETCH_ASSOC);
+		if ($cartQueryResult->rowCount()) {
 
-    
-}
+			// Session variable cart contains productId as key and product info as value
+			forEach($cartQueryResult->fetchAll(PDO::FETCH_ASSOC) as $product) {
+				$_SESSION["cart"]["{$product["productId"]}"] = array("size"=>"{$product["size"]}",
+																															"colour"=>"{$product["colour"]}",
+																															"quantity"=>"{$product["quantity"]}",
+																															"unitPrice"=>"{$product["unitPrice"]}",
+																															"discount"=>"{$product["discount"]}",
+																															"prodName"=>"{$product["prodName"]}",
+																															"picture"=>"{$product["picture"]}");
+			}
+		}
+	}
+
+	// Checking if user is already a customer
+  $isCustomer = $conn->query("SELECT * FROM customer WHERE username = {$conn->quote($username)}");
 
   // Form submitted
   if ($_SERVER["REQUEST_METHOD"] == "POST") {   
@@ -37,48 +54,53 @@ if (!isset($_SESSION["username"])) {
       $creditcardnum = $_POST["creditcardnum"];
     }
     if (empty($_POST["creditcardpin"])) {
-        $creditcardnumErr = "Credit card pin is required.";
-      } else {
-        $creditcardpin = $_POST["creditcardpin"];
-      }
+      $creditcardnumErr = "Credit card pin is required.";
+		} else {
+			$creditcardpin = $_POST["creditcardpin"];
+		}
 
-      //Insert Into Order and Order Details
+		// Form valid: Insert Into Order and Order Details, Update inventory
+		if($creditcardnumErr  == "" && $creditcardpinErr == "") {
 
-      if( $creditcardnumErr  == "" && $creditcardpinErr == "")
-      {
-        require_once "includes/db_connect.php";
+			// $creditCardId = $conn->query("SELECT creditCardId 
+			// 													 		FROM paymentinfo 
+			// 													 		WHERE  creditCardNo =  $creditcardnum ")
+			// 											->fetch(PDO::FETCH_ASSOC)['creditCardId'];
+		
+			$conn->exec("INSERT INTO orders (status, orderDate, creditCardNo, username )
+									 VALUES ( 'Pending', {$conn->quote(date("Y-m-d"))}, 1, {$conn->quote($username)} );");
 
-        $paymentId = $conn->query("SELECT paymentId FROM paymentinfo WHERE  creditCardNo =  $creditcardnum ")->fetch(PDO::FETCH_ASSOC)['paymentId'];
-      
-        $sInsert = "INSERT INTO order(status,orderDate,paymentId, username )
-             VALUES ( {$conn->quote($status)}, {$conn->quote(date("Y-m-d"))} ,
-             {$conn->quote($paymentId)}, {$conn->quote($username)});";
-        #echo $sInsert;
+			$orderId = $conn->query("SELECT MAX(orderId) AS orderId 
+															 FROM orders 
+															 WHERE  username = {$conn->quote($username)}")
+                      ->fetch(PDO::FETCH_ASSOC)['orderId'];
 
-        $orderId = $conn->query("SELECT MAX (orderId FROM orders WHERE  username =  {$conn->quote($username)} ")->fetch(PDO::FETCH_ASSOC)['orderId'];
+			foreach($_SESSION["cart"] as $productId => $product) {
 
-        foreach($_SESSION["cartinfo"] as $product) {
-          $sInsert = "INSERT INTO orderitems(orderId,productId,size,colour, quantity,unitprice,discount )
-          VALUES ( {$conn->quote($orderId)}, {$conn->quote($product["productId"])}, {$conn->quote($product["size"])} ,
-          {$conn->quote($product["colour"])}, {$conn->quote($product["quantity"])} , {$conn->quote($product["unitprice"])} , {$conn->quote($product["discount"])});";
-          $conn->exec($sInsert);
+				$orderItemsQuery = "INSERT INTO orderitems (orderId, productId, size, colour, quantity, unitprice, discount)
+														VALUES ( {$orderId}, {$productId}, {$conn->quote($product["size"])},
+														 			 {$conn->quote($product["colour"])}, {$product["quantity"]}, {$product["unitPrice"]}, 
+														 			 {$product["discount"]} );";
 
-         $updateinventory =  "UPDATE inventory
-           SET stockLevel  = stockLevel - {$product["quantity"]}
-           WHERE productId = {$product["productId"]}
-           AND  size = {$product["size"]}
-           AND colour = {$product["colour"]};";
-           $conn->exec($updateinventory);
-      }
+				$conn->exec($orderItemsQuery);
 
+				$updateInventory = "UPDATE inventory
+														SET stockLevel  = stockLevel - {$product["quantity"]}
+														WHERE productId = {$productId}
+														AND  size = {$conn->quote($product["size"])}
+														AND colour = {$conn->quote($product["colour"])};";
+				$conn->exec($updateInventory);
+		  }
+			// Checkout complete; Empty user cart in db
+			$conn->exec("DELETE FROM cart WHERE username = {$conn->quote($username)}");
+			unset($_SESSION["cart"]);
 
-        
-      $conn=null;    
-      }//end if	
-    }//end if
-
-
- ?>
+			header("Location: index.html");
+			die();
+		}	
+	}
+	$conn = null;    
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -97,145 +119,148 @@ if (!isset($_SESSION["username"])) {
 
 <body>
 
-    <nav class="mynavbar"></nav>
+	<nav class="mynavbar"></nav>
 
-    <div class="main-content">
-    
-        
-        <div class="wrapper order-summary">
-            <h1>Order Summary</h1>
-            <div class="project">
-                <div class="shop">
-                    <div class="box">
-                        <img src="img/Men/Sportswear/68.png">
-                        <div class="content">
-                            <h3>Men's Sportswear</h3>
-                            <h4><span class="product-colour">Colour: Red</span><span class="product-size">Size: S</span>Price: Rs 500</h4>
-                            <p class="unit">Quantity: <input type="number" name="" value="2"></p>
-                        </div>
-                    </div>
-                    <div class="box">
-                        <img src="img/Women/Dress/79.png">
-                        <div class="content">
-                            <h3>Woman's Dress</h3>
-	                        <h4><span class="product-colour">Colour: Blue</span><span class="product-size">Size: L</span>Price: Rs 400</h4>
-                            <p class="unit">Quantity: <input type="number" name="" value="1"></p>
-                        </div>
-                    </div>
-                    <div class="box">
-                        <img src="img/Women/Shirts_Blouse/4.png">
-                        <div class="content">
-                            <h3>Woman's Blouse</h3>
-							<h4><span class="product-colour">Colour: Yellow</span><span class="product-size">Size: M</span>Price: Rs 300</h4>
-                            <p class="unit">Quantity: <input type="number" name="" value="0"></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="right-bar">
-				    <p><span>Subtotal</span> <span class="subtotal">Rs 1200</span></p>
-					<hr>
-					<p><span>Tax (5%)</span> <span class="tax">Rs 60</span></p>
-					<hr>
-					<p><span>Total</span> <span class="total">Rs 1260</span></p>
-                    <a href="cart.html" class="checkout-link"><i class="fa fa-shopping-cart"></i>Back to cart</a>
-                </div>
-            </div>
-        </div>
+	<div class="main-content">
+	
+		<div class="wrapper order-summary">
+			<h1>Order Summary</h1>
+			<div class="project">
+				<div class="shop">
 
-         <div class="wrapper payment-form">
-            <h2>Payment Form</h2>
-            <form method="post" action="<?php echo $_SERVER["PHP_SELF"] ;?>" >
-                <!--Account Information Start-->
-                <h4>Account</h4>
-                <div class="input_group">
-                    <div class="input_box">
-                        <input type="text" placeholder="First Name" required class="name">
-                        <i class="fa fa-user icon"></i>
-                    </div>
-                    <div class="input_box">
-                        <input type="text" placeholder="Last Name" required class="name">
-                        <i class="fa fa-user icon"></i>
-                    </div>
-                </div>
-                <div class="input_group">
-                    <div class="input_box">
-                        <input type="email" placeholder="Email Address" required class="name">
-                        <i class="fa fa-envelope icon"></i>
-                    </div>
-                </div>
-                <div class="input_group">
-                    <div class="input_box">
-                        <input type="text" placeholder="Telephone number" required class="name">
-                        <i class="fa fa-phone icon"></i>
-                    </div>
-                </div>
-                <div class="input_group">
-                    <div class="input_box">
-                        <input type="text" placeholder="Address" required class="name">
-                        <i class="fa fa-map-marker icon" aria-hidden="true"></i>
-                    </div>
-                </div>
-                
-                <!--Account Information End-->
-                
-                
-                <!--DOB & Gender Start-->
-                <div class="input_group">
-                    <div class="input_box">
-                        <h4>Date Of Birth</h4>
-                        <input type="text" placeholder="DD" required class="dob">
-                        <input type="text" placeholder="MM" required class="dob">
-                        <input type="text" placeholder="YYYY" required class="dob">
-                    </div>
-                    <div class="input_box">
-                        <h4>Gender</h4>
-                        <input type="radio" name="gender" class="radio" id="b1" checked>
-                        <label for="b1">Male</label>
-                        <input type="radio" name="gender" class="radio" id="b2">
-                        <label for="b2">Female</label>
-                    </div>
-                </div>
-                <!--DOB & Gender End-->
-                
-                
-                <!--Payment Details Start-->
-                <div class="input_group">
-                    <div class="input_box">
-                        <h4>Payment Details</h4>
-                        <input type="radio" name="pay" class="radio" id="bc1" checked>
-                        <label for="bc1"><span>
-                            <i class="fa fa-cc-visa"></i>Credit Card</span></label>
-                            <input type="radio" name="pay" class="radio" id="bc2">
-                            <label for="bc2"><span>
-                                <i class="fa fa-cc-paypal"></i>Paypal</span></label>
-                    </div>
-                </div>
-                <div class="input_group">
-                    <div class="input_box">
-                        <input type="tel" type="number" name="creditcardnum" class="name" placeholder="Card Number 1111-2222-3333-4444" required>
-                        <i class="fa fa-credit-card icon"></i>
-                    </div>
-                </div>
-                <div class="input_box">
-                    <input type="text" name="creditcardpin" placeholder="Credit PIN" required class="name">
-                    <i class="fa fa-lock icon" aria-hidden="true"></i>
-                </div>
-                <!--Payment Details End-->
-                
-                <div class="input_group">
-                    <div class="input_box">
-                        <input type="submit" value = "PAY NOW"></input>
-                    </div>
-                </div>    
-            </form>
-        </div>
-                
-    </div>
+					<?php
+						$subTotal = 0;
 
-    <footer></footer>
+						// Display cart items
+						foreach($_SESSION["cart"] as $productId => $product) {
+							$subTotal += ($product["unitPrice"] * $product["quantity"]);
 
-    <script src="js/nav.js"></script>
-    <script src="js/footer.js"></script>
+							echo "<div class='box'>";
+							echo "<img src='img/{$product["picture"]}'>";
+							echo "<div class='content'>";
+							echo "<h3>{$product["prodName"]}</h3>";
+							echo "<h4><span class='product-colour'>Colour: {$product["colour"]}</span><span class='product-size'>Size: {$product["size"]}</span>Price: Rs {$product["unitPrice"]}</h4>";
+							echo "<p class='unit'>Quantity: <input type='number' name='quantity' value='{$product["quantity"]}' max='100' min='1'></p>";
+							echo "</div>";
+							echo "</div>";	
+						}
+
+						$tax = $subTotal * 0.05;
+						$total = $subTotal + $tax;
+
+						// Cart empty
+						if (!$_SESSION["cart"]) {
+							echo "<div class='box'>";
+							echo "<div class='content'>";
+							echo "<h3 style='text-align: center;'>Cart Empty</h3>";
+							echo "</div>";						
+							echo "</div>";						
+						}
+					?>
+
+				</div>
+				<div class="right-bar">
+					<div <?php if (!$_SESSION["cart"]) echo "hidden"; ?>>
+						<p><span>Subtotal</span> <span class="subtotal">Rs <?php echo $subTotal; ?></span></p>
+						<hr>
+						<p><span>Tax (5%)</span> <span class="tax">Rs <?php echo $tax; ?></span></p>
+						<hr>
+						<p><span>Total</span> <span class="total">Rs <?php echo $total; ?></span></p>
+					</div>
+					<a href="cart.php" class="checkout-link"><i class="fa fa-shopping-cart"></i>Back to cart</a>						
+				</div>
+			</div>				
+		</div>
+
+		<div class="wrapper payment-form" <?php if (!$_SESSION["cart"]) echo "hidden"; ?>>
+			<h2>Payment Form</h2>
+			<form method="POST" action="<?php echo $_SERVER["PHP_SELF"] ;?>">
+				<!--Account Information Start-->
+				<div <?php if ($isCustomer) echo "hidden"; ?>>
+
+					<h4>Account</h4>
+					<div class="input_group">
+						<div class="input_box">
+							<input type="text" placeholder="First Name" <?php if ($isCustomer) echo "disabled"; ?> required class="name">
+							<i class="fa fa-user icon"></i>
+						</div>
+						<div class="input_box">
+							<input type="text" placeholder="Last Name" <?php if ($isCustomer) echo "disabled"; ?> required class="name">
+							<i class="fa fa-user icon"></i>
+						</div>
+					</div>
+					<div class="input_group">
+						<div class="input_box">
+							<input type="email" placeholder="Email Address" <?php if ($isCustomer) echo "disabled"; ?> required class="name">
+							<i class="fa fa-envelope icon"></i>
+						</div>
+					</div>
+					<div class="input_group">
+						<div class="input_box">
+							<input type="text" placeholder="Telephone number" <?php if ($isCustomer) echo "disabled"; ?> required class="name">
+							<i class="fa fa-phone icon"></i>
+						</div>
+					</div>
+					<div class="input_group">
+						<div class="input_box">
+							<input type="text" placeholder="Address" <?php if ($isCustomer) echo "disabled"; ?> required class="name">
+							<i class="fa fa-map-marker icon" aria-hidden="true"></i>
+						</div>
+					</div>
+					
+					<!--DOB & Gender Start-->
+					<div class="input_group">
+						<div class="input_box">
+							<h4>Date Of Birth</h4>
+							<input type="text" placeholder="DD" <?php if ($isCustomer) echo "disabled"; ?> required class="dob">
+							<input type="text" placeholder="MM" <?php if ($isCustomer) echo "disabled"; ?> required class="dob">
+							<input type="text" placeholder="YYYY" <?php if ($isCustomer) echo "disabled"; ?> required class="dob">
+						</div>
+						<div class="input_box">
+							<h4>Gender</h4>
+							<input type="radio" name="gender" <?php if ($isCustomer) echo "disabled"; ?> class="radio" id="b1" checked>
+							<label for="b1">Male</label>
+							<input type="radio" name="gender" <?php if ($isCustomer) echo "disabled"; ?> class="radio" id="b2">
+							<label for="b2">Female</label>
+						</div>
+					</div>
+				</div>
+				<!--DOB & Gender End-->
+				<!--Account Information End-->
+				
+				<!--Payment Details Start-->
+				<div class="input_group">
+					<div class="input_box">
+						<h4>Payment Details</h4>
+					</div>
+				</div>
+				<div class="input_group">
+					<div class="input_box">
+						<input type="tel" type="number" name="creditcardnum" class="name" placeholder="Card Number 1111-2222-3333-4444" required>
+						<i class="fa fa-credit-card icon"></i>
+					</div>
+				</div>
+				<div class="input_group">
+					<div class="input_box">
+						<input type="text" name="creditcardpin" placeholder="Credit PIN" required class="name">
+						<i class="fa fa-lock icon" aria-hidden="true"></i>
+					</div>
+				</div>	
+				<!--Payment Details End-->
+				
+				<div class="input_group">
+					<div class="input_box">
+						<button type="submit" id="submit-btn" value="submit">Place Order</button>
+					</div>
+				</div>    
+			</form>
+		</div>			    
+	</div>
+
+	<footer></footer>
+
+	<script src="js/nav.js"></script>
+	<script src="js/footer.js"></script>
 
 </body>
 </html>
